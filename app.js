3621d3f042d5eec591fa4c421955ad1b4ffced78
ara <zowsky24@gmail.com>
2020-09-27 21:46:16 +0900

    Second commit

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(require('connect-history-api-fallback')())
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  next(createError(404)); 
 });
 
 // error handler
