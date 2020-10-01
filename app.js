78e6bc553e1213a3c11012399c4e0ff54a8c22e9
ara <zowsky24@gmail.com>
2020-10-02 00:34:40 +0900

    Join

--- app.js
+++ app.js
@@ -4,16 +4,18 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var http = require('http');
+var mysql = require('mysql');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var moviesRouter = require('./routes/movies');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-// app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3000);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,6 +25,47 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api/movies', moviesRouter);
+
+
+// Connection 객체 생성 
+var pool = mysql.createPool({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',   
+  password: 'whdkfk1!',
+  database: 'test_schema'  
+});  
+
+
+// insert
+app.post('/regist', function (req, res) {
+
+  // Connect
+  pool.getConnection(function(err, conn) {
+    if(err) {
+      console.log(err);
+      return;
+    }
+    console.log('데이터베이스 연결 스레드 아이디 : ' + conn.threadId);
+
+    var user = {
+      userid: req.body.userid,
+      name: req.body.name,
+      address: req.body.address
+    };
+
+    var exec = conn.query('insert into users set ?', user, function(err, rows) {
+      if(err) { 
+        console.log('query문 실행 중 오류!! : ' + exec.sql + ' : ' + err);
+      }else {
+        console.log('query문 성공 : ' + exec.sql);
+        res.status(200).send('success');
+      }
+    });
+  });
+});
+
 
 app.use(require('connect-history-api-fallback')())
 
@@ -42,8 +85,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.use(function(req, res, err) {
-  console.log(req.env);
+http.createServer(app).listen(app.get('port'), function(req,res) {
+  console.log('익스프레스 서버 시작 : ' + app.get('port'));
 })
 
 module.exports = app;
