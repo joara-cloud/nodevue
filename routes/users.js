bcdd51071f31f2b756b6d2f542348969aa2a43dc
ara <joarario92@gmail.com>
2020-10-03 20:10:17 +0900

    Add Signup

--- routes/users.js
+++ routes/users.js
@@ -1,91 +1,70 @@
 const express = require('express');
+const mysql = require('mysql');
+const bcrypt = require('bcrypt');
 const router = express.Router();
 const app = express();
 
-// 다음은 백엔드의 routes/users.js에 넣어줄 부분입니다.
-// 입력 받은 데이터를 user 객체에 저장한 뒤, DB에 동일한 아이디를 가진 데이터가 없다면 user를 DB에 넣고 그렇지 않다면 에러 메시지를 제공합니다.
-// user 데이터를 DB에 넣을 때, bcrypt라는 패키지를 사용하여 비밀번호를 암호화합니다.
+var pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  port: 3306,
+  password: 'whdkfk1!',
+  database: 'test_schema'
+})
 
 router.post('/signup', function(req, res, next) {
+  console.log('signup post 요청 실행됨');
 
-  const user = {
+  let user = { // 입력 받은 데이터를 user 객체에 저장
     userid: req.body.user.userid,
     name: req.body.user.name,
     password: req.body.user.password
   };
+  console.log('user 객체 세팅완료');
 
   pool.getConnection(function(err, conn) {
     if(err) {
-      console.log(err.stack);
+      console.log('연결객체 내부에서 오류 : ' + err.stack);
       return;
     }else {
-      alert(conn);
-    }
-  })
+      console.log('연결객체 연결 성공!');
+      
+      const exec = conn.query("select userid from users where userid = '" + user.userid + "'", function(err, row) {
+        if(err) {
+          console.log('쿼리문 실행중 오류! : '+err);
+          return;
+        }else {
+          if(row.length > 0) { // DB에 동일한 아이디를 가진 데이터가 있다면 에러 메시지를 제공합니다.
+            res.json({
+              success: false,
+              msg: '이미 사용중인 아이디 입니다.'
+            });
+          }else { // DB에 동일한 아이디를 가진 데이터가 없다면 user를 DB에 넣고
 
-  // connection.query('SELECT userid FROM users WHERE userid = "' + user.userid + '"', function (err, row) {
-  //   console.log(row[0]);
-  //   if (row[0] == undefined){ //  동일한 아이디가 없을경우,
-  //     const salt = bcrypt.genSaltSync();
-  //     const encryptedPassword = bcrypt.hashSync(user.password, salt);
-  //     connection.query('INSERT INTO users (userid,name,password) VALUES ("' + user.userid + '","' + user.name + '","' + encryptedPassword + '")', user, function (err, row2) {
-  //       if (err) throw err;
-  //     });
-  //     res.json({
-  //       success: true,
-  //       msg: 'Sing Up Success!'
-  //     })
-  //   }
-  //   else {
-  //     res.json({
-  //       success: false,
-  //       msg: 'Sign Up Failed Please use anoter ID'
-  //     })
-  //   }
-  // });
-  
+            const salt = bcrypt.genSaltSync();
+            const encryptedPassword = bcrypt.hashSync(user.password, salt); // user 데이터를 DB에 넣을 때, bcrypt라는 패키지를 사용하여 비밀번호를 암호화합니다. (bcrypt 모듈 설치하고 import해아함!)
+
+            user.password = encryptedPassword;
+
+            let intoExec = conn.query('INSERT INTO users set ?', user, function(err, row) {
+              if(err) {
+                console.log('insert 실행 중 오류 : ' + err);
+              }
+
+              console.log(intoExec.sql);
+
+            });
+            
+            res.json({
+              success: true,
+              msg: '회원가입이 완료되었습니다.'
+            });
+          }
+        }
+      });
+    }
+  });
 });
 
 
 module.exports = router;
-
-
-
-
-// router.post('/singup', function(req, res) {
-//   const user = {
-//     userid: req.body.user.userid,
-//     name: req.body.user.name,
-//     password: req.body.user.password
-//   };
-
-//   console.log(user);
-// });
-
-// router.post('/signUp', function (req, res) {
-//   const user = {
-//     'userid': req.body.user.userid,
-//     'name': req.body.user.name,
-//     'password': req.body.user.password
-//   };
-//   connection.query('SELECT userid FROM users WHERE userid = "' + user.userid + '"', function (err, row) {
-//     if (row[0] == undefined){ //  동일한 아이디가 없을경우,
-//       const salt = bcrypt.genSaltSync();
-//       const encryptedPassword = bcrypt.hashSync(user.password, salt);
-//       connection.query('INSERT INTO users (userid,name,password) VALUES ("' + user.userid + '","' + user.name + '","' + encryptedPassword + '")', user, function (err, row2) {
-//         if (err) throw err;
-//       });
-//       res.json({
-//         success: true,
-//         message: 'Sing Up Success!'
-//       })
-//     }
-//     else {
-//       res.json({
-//         success: false,
-//         message: 'Sign Up Failed Please use anoter ID'
-//       })
-//     }
-//   });
-  
-// });
