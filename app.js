06a1a5fa44bea4137ea6bd67960e74b04d368b51
ara <joarario92@gmail.com>
2020-10-03 23:08:02 +0900

    commit

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.set('port', process.env.PORT || 3000);
+// app.set('port', process.env.PORT || 3000);
 
 app.use(logger('dev'));
 app.use(express.json());
