7c89589d7cbaf4171234fca5633aa070a29b6c09
ara <joarario92@gmail.com>
2020-10-07 23:04:46 +0900

    sdf

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -64,7 +64,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+ 
 // http.createServer(app).listen(app.get('port'), function(req,res) {
 //   console.log('익스프레스 서버 시작 : ' + app.get('port'));
 // });
