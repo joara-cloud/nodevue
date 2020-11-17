54e4e64c3c3af0e9ec03f8ecda81999dfa9e3c7e
ara <joarario92@gmail.com>
2020-11-18 00:14:41 +0900

    Studying

---
+++ public/js/chunk-2d22d746.54bf5436.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(t,n,e){"use strict";e.r(n);var c=function(){var t=this,n=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("div",{attrs:{id:"content"}},[e("div",{staticClass:"about"},[t._v(" About ")])])}],u=e("2877"),a={},r=Object(u["a"])(a,c,s,!1,null,null,null);n["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d22d746.54bf5436.js.map
